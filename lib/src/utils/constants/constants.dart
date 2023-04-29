6996831e4dccf9ec06cfc628e5fe8f4dcfc86081
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-29 10:05:58 +0700

    Update constants.dart

--- lib/src/utils/constants/constants.dart
+++ lib/src/utils/constants/constants.dart
@@ -1,4 +1,8 @@
 class Constants {
   static const appTitle = 'CareerCrush';
   static const fontFamily = 'Poppins';
+
+  /* Spaces */
+  static const double horizontalPadding = 30;
+  static const double welcomeTopPadding = 100;
 }
