30ddd415d446a6bc260b554be63d95a4acd7a7a1
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-29 13:06:16 +0700

    Update index_screen.dart

--- lib/src/presentation/views/index_screen.dart
+++ lib/src/presentation/views/index_screen.dart
@@ -12,28 +12,45 @@ class IndexScreen extends StatelessWidget {
       body: PageView(
         children: AppRouter.bottomNav.map((e) => e.screen).toList(),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          showUnselectedLabels: true,
-          selectedItemColor: ColorName.lavender,
-          unselectedItemColor: Colors.grey.shade400,
-          selectedIconTheme: const IconThemeData(color: ColorName.lavender),
-          selectedFontSize: 12,
-          type: BottomNavigationBarType.fixed,
-          enableFeedback: false,
-          items: AppRouter.bottomNav
-              .map((e) => BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    e.icon,
-                    colorFilter:
-                        ColorFilter.mode(Colors.grey.shade400, BlendMode.srcIn),
-                  ),
-                  activeIcon: SvgPicture.asset(
-                    e.icon,
-                    colorFilter: const ColorFilter.mode(
-                        ColorName.lavender, BlendMode.srcIn),
-                  ),
-                  label: e.label))
-              .toList()),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, -5),
+                  blurRadius: 10,
+                  spreadRadius: 0,
+                  color: Colors.black12)
+            ],
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        child: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            showUnselectedLabels: true,
+            selectedItemColor: ColorName.lavender,
+            unselectedItemColor: Colors.grey.shade400,
+            selectedIconTheme: const IconThemeData(color: ColorName.lavender),
+            selectedFontSize: 10,
+            unselectedFontSize: 10,
+            type: BottomNavigationBarType.fixed,
+            enableFeedback: false,
+            elevation: 0,
+            items: AppRouter.bottomNav
+                .map((e) => BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      e.icon,
+                      colorFilter: ColorFilter.mode(
+                          Colors.grey.shade400, BlendMode.srcIn),
+                    ),
+                    activeIcon: SvgPicture.asset(
+                      e.icon,
+                      colorFilter: const ColorFilter.mode(
+                          ColorName.lavender, BlendMode.srcIn),
+                    ),
+                    label: e.label))
+                .toList()),
+      ),
     );
   }
 }
