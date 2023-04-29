ded1367d240984c73e726d41a612403c1b07656e
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-29 10:05:18 +0700

    Create page_scaffold.dart

---
+++ lib/src/presentation/widgets/scaffolds/page_scaffold.dart
@@ -0,0 +1,31 @@
+import 'package:career_crush/gen/assets.gen.dart';
+import 'package:career_crush/src/presentation/widgets/logo/logo.dart';
+import 'package:career_crush/src/utils/constants/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PageScaffold extends Scaffold {
+  PageScaffold({Key? key, Widget? body, String? title, bool useAppBar = true})
+      : super(
+          key: key,
+          backgroundColor: Colors.white,
+          appBar: !useAppBar
+              ? null
+              : AppBar(
+                  backgroundColor: Colors.white,
+                  elevation: 0,
+                  title: const Logo(),
+                  actions: [
+                    IconButton(
+                      icon: SvgPicture.asset(Assets.images.icons.apps.setting),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+          body: Container(
+            padding: const EdgeInsets.symmetric(
+                horizontal: Constants.horizontalPadding),
+            child: body,
+          ),
+        );
+}
