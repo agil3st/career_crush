6c5e380a3bee350d5fd86c16b43117e4b2d1b166
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-29 10:37:20 +0700

    Update logo.dart

--- lib/src/presentation/widgets/logo/logo.dart
+++ lib/src/presentation/widgets/logo/logo.dart
@@ -1,15 +1,11 @@
 import 'package:career_crush/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Logo extends StatelessWidget {
   const Logo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      Assets.images.logo,
-      width: 100,
-    );
+    return Assets.images.logo.svg(width: 100);
   }
 }
