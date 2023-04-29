607760e72f2fed89b321b07af6f86b6772caa30e
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-29 10:05:14 +0700

    Add assets

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -12,11 +12,351 @@ import 'package:flutter/widgets.dart';
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
+  $AssetsImagesIconsGen get icons => const $AssetsImagesIconsGen();
+  $AssetsImagesIllustrationsGen get illustrations =>
+      const $AssetsImagesIllustrationsGen();
+
   /// File path: assets/images/logo.svg
   String get logo => 'assets/images/logo.svg';
 
   /// List of all assets
   List<String> get values => [logo];
+}
+
+class $AssetsImagesIconsGen {
+  const $AssetsImagesIconsGen();
+
+  $AssetsImagesIconsAppsGen get apps => const $AssetsImagesIconsAppsGen();
+  $AssetsImagesIconsBusinessGen get business =>
+      const $AssetsImagesIconsBusinessGen();
+}
+
+class $AssetsImagesIllustrationsGen {
+  const $AssetsImagesIllustrationsGen();
+
+  /// File path: assets/images/illustrations/welcome.png
+  AssetGenImage get welcome =>
+      const AssetGenImage('assets/images/illustrations/welcome.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [welcome];
+}
+
+class $AssetsImagesIconsAppsGen {
+  const $AssetsImagesIconsAppsGen();
+
+  /// File path: assets/images/icons/apps/Calender.svg
+  String get calender => 'assets/images/icons/apps/Calender.svg';
+
+  /// File path: assets/images/icons/apps/Camera.svg
+  String get camera => 'assets/images/icons/apps/Camera.svg';
+
+  /// File path: assets/images/icons/apps/Cart.svg
+  String get cart => 'assets/images/icons/apps/Cart.svg';
+
+  /// File path: assets/images/icons/apps/Chat.svg
+  String get chat => 'assets/images/icons/apps/Chat.svg';
+
+  /// File path: assets/images/icons/apps/Clock.svg
+  String get clock => 'assets/images/icons/apps/Clock.svg';
+
+  /// File path: assets/images/icons/apps/Document.svg
+  String get document => 'assets/images/icons/apps/Document.svg';
+
+  /// File path: assets/images/icons/apps/Download.svg
+  String get download => 'assets/images/icons/apps/Download.svg';
+
+  /// File path: assets/images/icons/apps/Folder.svg
+  String get folder => 'assets/images/icons/apps/Folder.svg';
+
+  /// File path: assets/images/icons/apps/Home.svg
+  String get home => 'assets/images/icons/apps/Home.svg';
+
+  /// File path: assets/images/icons/apps/Like.svg
+  String get like => 'assets/images/icons/apps/Like.svg';
+
+  /// File path: assets/images/icons/apps/Lock.svg
+  String get lock => 'assets/images/icons/apps/Lock.svg';
+
+  /// File path: assets/images/icons/apps/Mail.svg
+  String get mail => 'assets/images/icons/apps/Mail.svg';
+
+  /// File path: assets/images/icons/apps/Maps.svg
+  String get maps => 'assets/images/icons/apps/Maps.svg';
+
+  /// File path: assets/images/icons/apps/Menu.svg
+  String get menu => 'assets/images/icons/apps/Menu.svg';
+
+  /// File path: assets/images/icons/apps/Microphone.svg
+  String get microphone => 'assets/images/icons/apps/Microphone.svg';
+
+  /// File path: assets/images/icons/apps/Minus.svg
+  String get minus => 'assets/images/icons/apps/Minus.svg';
+
+  /// File path: assets/images/icons/apps/Notification.svg
+  String get notification => 'assets/images/icons/apps/Notification.svg';
+
+  /// File path: assets/images/icons/apps/Picture.svg
+  String get picture => 'assets/images/icons/apps/Picture.svg';
+
+  /// File path: assets/images/icons/apps/Plus.svg
+  String get plus => 'assets/images/icons/apps/Plus.svg';
+
+  /// File path: assets/images/icons/apps/Profile.svg
+  String get profile => 'assets/images/icons/apps/Profile.svg';
+
+  /// File path: assets/images/icons/apps/Save.svg
+  String get save => 'assets/images/icons/apps/Save.svg';
+
+  /// File path: assets/images/icons/apps/Search.svg
+  String get search => 'assets/images/icons/apps/Search.svg';
+
+  /// File path: assets/images/icons/apps/Send.svg
+  String get send => 'assets/images/icons/apps/Send.svg';
+
+  /// File path: assets/images/icons/apps/Setting.svg
+  String get setting => 'assets/images/icons/apps/Setting.svg';
+
+  /// File path: assets/images/icons/apps/Unlock.svg
+  String get unlock => 'assets/images/icons/apps/Unlock.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        calender,
+        camera,
+        cart,
+        chat,
+        clock,
+        document,
+        download,
+        folder,
+        home,
+        like,
+        lock,
+        mail,
+        maps,
+        menu,
+        microphone,
+        minus,
+        notification,
+        picture,
+        plus,
+        profile,
+        save,
+        search,
+        send,
+        setting,
+        unlock
+      ];
+}
+
+class $AssetsImagesIconsBusinessGen {
+  const $AssetsImagesIconsBusinessGen();
+
+  /// File path: assets/images/icons/business/Approved.svg
+  String get approved => 'assets/images/icons/business/Approved.svg';
+
+  /// File path: assets/images/icons/business/Balance.svg
+  String get balance => 'assets/images/icons/business/Balance.svg';
+
+  /// File path: assets/images/icons/business/Bank.svg
+  String get bank => 'assets/images/icons/business/Bank.svg';
+
+  /// File path: assets/images/icons/business/Brainstorming.svg
+  String get brainstorming => 'assets/images/icons/business/Brainstorming.svg';
+
+  /// File path: assets/images/icons/business/Calculator.svg
+  String get calculator => 'assets/images/icons/business/Calculator.svg';
+
+  /// File path: assets/images/icons/business/Commission.svg
+  String get commission => 'assets/images/icons/business/Commission.svg';
+
+  /// File path: assets/images/icons/business/Contact Book.svg
+  String get contactBook => 'assets/images/icons/business/Contact Book.svg';
+
+  /// File path: assets/images/icons/business/Deadline.svg
+  String get deadline => 'assets/images/icons/business/Deadline.svg';
+
+  /// File path: assets/images/icons/business/Delete File.svg
+  String get deleteFile => 'assets/images/icons/business/Delete File.svg';
+
+  /// File path: assets/images/icons/business/Document.svg
+  String get document => 'assets/images/icons/business/Document.svg';
+
+  /// File path: assets/images/icons/business/E-Commerce.svg
+  String get eCommerce => 'assets/images/icons/business/E-Commerce.svg';
+
+  /// File path: assets/images/icons/business/Education.svg
+  String get education => 'assets/images/icons/business/Education.svg';
+
+  /// File path: assets/images/icons/business/Email.svg
+  String get email => 'assets/images/icons/business/Email.svg';
+
+  /// File path: assets/images/icons/business/Financial Plan.svg
+  String get financialPlan => 'assets/images/icons/business/Financial Plan.svg';
+
+  /// File path: assets/images/icons/business/Graphic Design.svg
+  String get graphicDesign => 'assets/images/icons/business/Graphic Design.svg';
+
+  /// File path: assets/images/icons/business/Income.svg
+  String get income => 'assets/images/icons/business/Income.svg';
+
+  /// File path: assets/images/icons/business/Info.svg
+  String get info => 'assets/images/icons/business/Info.svg';
+
+  /// File path: assets/images/icons/business/Insurance.svg
+  String get insurance => 'assets/images/icons/business/Insurance.svg';
+
+  /// File path: assets/images/icons/business/Invoice.svg
+  String get invoice => 'assets/images/icons/business/Invoice.svg';
+
+  /// File path: assets/images/icons/business/Location.svg
+  String get location => 'assets/images/icons/business/Location.svg';
+
+  /// File path: assets/images/icons/business/M Banking.svg
+  String get mBanking => 'assets/images/icons/business/M Banking.svg';
+
+  /// File path: assets/images/icons/business/Meeting.svg
+  String get meeting => 'assets/images/icons/business/Meeting.svg';
+
+  /// File path: assets/images/icons/business/Money Growth.svg
+  String get moneyGrowth => 'assets/images/icons/business/Money Growth.svg';
+
+  /// File path: assets/images/icons/business/Money Saving.svg
+  String get moneySaving => 'assets/images/icons/business/Money Saving.svg';
+
+  /// File path: assets/images/icons/business/New File.svg
+  String get newFile => 'assets/images/icons/business/New File.svg';
+
+  /// File path: assets/images/icons/business/New Product.svg
+  String get newProduct => 'assets/images/icons/business/New Product.svg';
+
+  /// File path: assets/images/icons/business/News.svg
+  String get news => 'assets/images/icons/business/News.svg';
+
+  /// File path: assets/images/icons/business/Partnership.svg
+  String get partnership => 'assets/images/icons/business/Partnership.svg';
+
+  /// File path: assets/images/icons/business/Payment.svg
+  String get payment => 'assets/images/icons/business/Payment.svg';
+
+  /// File path: assets/images/icons/business/People Focus.svg
+  String get peopleFocus => 'assets/images/icons/business/People Focus.svg';
+
+  /// File path: assets/images/icons/business/Portfolio.svg
+  String get portfolio => 'assets/images/icons/business/Portfolio.svg';
+
+  /// File path: assets/images/icons/business/Premium.svg
+  String get premium => 'assets/images/icons/business/Premium.svg';
+
+  /// File path: assets/images/icons/business/Presentation.svg
+  String get presentation => 'assets/images/icons/business/Presentation.svg';
+
+  /// File path: assets/images/icons/business/Product Launch.svg
+  String get productLaunch => 'assets/images/icons/business/Product Launch.svg';
+
+  /// File path: assets/images/icons/business/Promotion.svg
+  String get promotion => 'assets/images/icons/business/Promotion.svg';
+
+  /// File path: assets/images/icons/business/Rejected.svg
+  String get rejected => 'assets/images/icons/business/Rejected.svg';
+
+  /// File path: assets/images/icons/business/Schedule.svg
+  String get schedule => 'assets/images/icons/business/Schedule.svg';
+
+  /// File path: assets/images/icons/business/Search.svg
+  String get search => 'assets/images/icons/business/Search.svg';
+
+  /// File path: assets/images/icons/business/Services.svg
+  String get services => 'assets/images/icons/business/Services.svg';
+
+  /// File path: assets/images/icons/business/Smart Solution.svg
+  String get smartSolution => 'assets/images/icons/business/Smart Solution.svg';
+
+  /// File path: assets/images/icons/business/Solution.svg
+  String get solution => 'assets/images/icons/business/Solution.svg';
+
+  /// File path: assets/images/icons/business/StartUp.svg
+  String get startUp => 'assets/images/icons/business/StartUp.svg';
+
+  /// File path: assets/images/icons/business/Statistic.svg
+  String get statistic => 'assets/images/icons/business/Statistic.svg';
+
+  /// File path: assets/images/icons/business/Stock.svg
+  String get stock => 'assets/images/icons/business/Stock.svg';
+
+  /// File path: assets/images/icons/business/Strategy.svg
+  String get strategy => 'assets/images/icons/business/Strategy.svg';
+
+  /// File path: assets/images/icons/business/Support.svg
+  String get support => 'assets/images/icons/business/Support.svg';
+
+  /// File path: assets/images/icons/business/Target.svg
+  String get target => 'assets/images/icons/business/Target.svg';
+
+  /// File path: assets/images/icons/business/Task.svg
+  String get task => 'assets/images/icons/business/Task.svg';
+
+  /// File path: assets/images/icons/business/Think Different.svg
+  String get thinkDifferent =>
+      'assets/images/icons/business/Think Different.svg';
+
+  /// File path: assets/images/icons/business/Time is Money.svg
+  String get timeIsMoney => 'assets/images/icons/business/Time is Money.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        approved,
+        balance,
+        bank,
+        brainstorming,
+        calculator,
+        commission,
+        contactBook,
+        deadline,
+        deleteFile,
+        document,
+        eCommerce,
+        education,
+        email,
+        financialPlan,
+        graphicDesign,
+        income,
+        info,
+        insurance,
+        invoice,
+        location,
+        mBanking,
+        meeting,
+        moneyGrowth,
+        moneySaving,
+        newFile,
+        newProduct,
+        news,
+        partnership,
+        payment,
+        peopleFocus,
+        portfolio,
+        premium,
+        presentation,
+        productLaunch,
+        promotion,
+        rejected,
+        schedule,
+        search,
+        services,
+        smartSolution,
+        solution,
+        startUp,
+        statistic,
+        stock,
+        strategy,
+        support,
+        target,
+        task,
+        thinkDifferent,
+        timeIsMoney
+      ];
 }
 
 class Assets {
