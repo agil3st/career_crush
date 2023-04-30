b5de9a012b31ee862be7e906360dbd46f5c6bbc6
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-30 11:58:39 +0700

    Update page_scaffold.dart

--- lib/src/presentation/widgets/scaffolds/page_scaffold.dart
+++ lib/src/presentation/widgets/scaffolds/page_scaffold.dart
@@ -1,15 +1,18 @@
-import 'package:career_crush/src/presentation/widgets/logo/logo.dart';
+import 'package:career_crush/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:go_router/go_router.dart';
 
 class PageScaffold extends Scaffold {
-  PageScaffold(
-      {Key? key,
-      Widget? body,
-      String? title,
-      bool useAppBar = true,
-      bool hideBackButton = true,
-      List<Widget>? actions})
-      : super(
+  PageScaffold({
+    Key? key,
+    Widget? body,
+    String? title,
+    Widget? titleWidget,
+    bool useAppBar = true,
+    bool hideBackButton = false,
+    List<Widget>? actions,
+  }) : super(
           key: key,
           backgroundColor: Colors.white,
           appBar: !useAppBar
@@ -17,11 +20,38 @@ class PageScaffold extends Scaffold {
               : AppBar(
                   backgroundColor: Colors.white,
                   elevation: 0,
-                  title: const Logo(),
-                  titleSpacing: 20,
+                  title: titleWidget ??
+                      Text(
+                        title ?? '',
+                        style: const TextStyle(
+                          color: ColorName.midnight,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                  titleSpacing: hideBackButton ? 20 : 0,
                   automaticallyImplyLeading: !hideBackButton,
+                  leading: const BackButton(),
+                  iconTheme: const IconThemeData(
+                    color: ColorName.midnight,
+                  ),
                   actions: actions,
                 ),
           body: body,
         );
+}
+
+class BackButton extends StatelessWidget {
+  const BackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        context.pop();
+      },
+      icon: const Icon(FeatherIcons.chevronLeft),
+      constraints: const BoxConstraints(),
+      padding: const EdgeInsets.only(bottom: 3),
+    );
+  }
 }
