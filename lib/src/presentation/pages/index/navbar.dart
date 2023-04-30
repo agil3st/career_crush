75a1644c4dd3eebbbc2b527ba0016eff33265e96
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-30 11:12:55 +0700

    Update navbar.dart

--- lib/src/presentation/pages/index/navbar.dart
+++ lib/src/presentation/pages/index/navbar.dart
@@ -19,43 +19,55 @@ class NavBarConsumer extends ConsumerWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10),
       decoration: const BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, -5),
-                blurRadius: 10,
-                spreadRadius: 0,
-                color: Colors.black12)
-          ],
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -5),
+            blurRadius: 10,
+            spreadRadius: 0,
+            color: Colors.black12,
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+        ),
+      ),
       child: BottomNavigationBar(
-          currentIndex: ref.watch(currentPageProvider),
-          onTap: animateToPage,
-          backgroundColor: Colors.transparent,
-          showUnselectedLabels: true,
-          selectedItemColor: ColorName.lavender,
-          unselectedItemColor: Colors.grey.shade400,
-          selectedIconTheme: const IconThemeData(color: ColorName.lavender),
-          selectedFontSize: 10,
-          unselectedFontSize: 10,
-          type: BottomNavigationBarType.fixed,
-          enableFeedback: false,
-          elevation: 0,
-          items: AppRouter.bottomNav
-              .map((e) => BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    e.icon,
-                    colorFilter:
-                        ColorFilter.mode(Colors.grey.shade400, BlendMode.srcIn),
+        currentIndex: ref.watch(currentPageProvider),
+        onTap: animateToPage,
+        backgroundColor: Colors.transparent,
+        showUnselectedLabels: true,
+        selectedItemColor: ColorName.lavender,
+        unselectedItemColor: Colors.grey.shade400,
+        selectedIconTheme: const IconThemeData(color: ColorName.lavender),
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        enableFeedback: false,
+        elevation: 0,
+        items: AppRouter.bottomNav
+            .map(
+              (e) => BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  e.icon,
+                  colorFilter: ColorFilter.mode(
+                    Colors.grey.shade400,
+                    BlendMode.srcIn,
                   ),
-                  activeIcon: SvgPicture.asset(
-                    e.icon,
-                    colorFilter: const ColorFilter.mode(
-                        ColorName.lavender, BlendMode.srcIn),
+                ),
+                activeIcon: SvgPicture.asset(
+                  e.icon,
+                  colorFilter: const ColorFilter.mode(
+                    ColorName.lavender,
+                    BlendMode.srcIn,
                   ),
-                  label: e.label))
-              .toList()),
+                ),
+                label: e.label,
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
