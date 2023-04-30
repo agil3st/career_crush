144cbe32b70989b593478757261bec519571a42b
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-30 08:41:15 +0700

    Update sign_up_screen.dart

--- lib/src/presentation/pages/auth/sign_up/sign_up_screen.dart
+++ lib/src/presentation/pages/auth/sign_up/sign_up_screen.dart
@@ -1,10 +1,12 @@
 import 'package:career_crush/gen/colors.gen.dart';
 import 'package:career_crush/src/presentation/widgets/buttons/full_button.dart';
 import 'package:career_crush/src/presentation/widgets/scaffolds/welcome_scaffold.dart';
+import 'package:career_crush/src/presentation/widgets/text_fields/custom_text_field.dart';
 import 'package:career_crush/src/utils/constants/constants.dart';
 import 'package:career_crush/src/utils/constants/routes.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:go_router/go_router.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -35,11 +37,66 @@ class SignUpScreen extends StatelessWidget {
                 ),
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              child: Column(
+                children: [
+                  CustomTextField(
+                    label: 'Email',
+                    hint: 'johndoe@email.com',
+                  ),
+                  CustomTextField.verticalSpacing,
+                  CustomTextField(
+                    label: 'Password',
+                    hint: 'Enter your password',
+                    obscureText: true,
+                  ),
+                  CustomTextField.verticalSpacing,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            constraints: const BoxConstraints(),
+                            padding: EdgeInsets.zero,
+                            icon: const Icon(
+                              FeatherIcons.checkSquare,
+                              size: 16,
+                            ),
+                          ),
+                          const SizedBox(width: 2),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 2),
+                            child: Text(
+                              'Remember me',
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      InkWell(
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            color: Colors.grey.shade400,
+                            fontSize: 12,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
             Column(
               children: [
                 Container(
                   width: double.infinity,
-                  padding: const EdgeInsets.fromLTRB(0, 60, 0, 20),
+                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 20),
                   child: FullButton(onPressed: () {}, label: 'Register'),
                 ),
                 RichText(
@@ -61,7 +118,7 @@ class SignUpScreen extends StatelessWidget {
                   ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
