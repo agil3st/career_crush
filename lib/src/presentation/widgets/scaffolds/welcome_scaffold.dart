bd2e5b70bc36f36d32361fe82383f2495625a62c
Agil Setiawan <agilsetiawan772@gmail.com>
2023-04-30 18:30:08 +0700

    Update welcome_scaffold.dart

--- lib/src/presentation/widgets/scaffolds/welcome_scaffold.dart
+++ lib/src/presentation/widgets/scaffolds/welcome_scaffold.dart
@@ -1,32 +1,55 @@
+import 'package:career_crush/gen/assets.gen.dart';
 import 'package:career_crush/src/presentation/widgets/logo/logo.dart';
 import 'package:career_crush/src/presentation/widgets/scaffolds/page_scaffold.dart';
 import 'package:career_crush/src/utils/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScaffold extends PageScaffold {
-  WelcomeScaffold(
-      {Key? key, required Widget body, String? title, bool useAppBar = true})
-      : super(
+  WelcomeScaffold({
+    Key? key,
+    required Widget body,
+    String? title,
+    bool useAppBar = true,
+    bool showSettingsButton = false,
+    bool showFooter = true,
+    EdgeInsets? padding,
+    List<Widget>? actions,
+  }) : super(
           key: key,
           titleWidget: const Logo(),
           hideBackButton: true,
+          actions: [
+            if (actions != null) ...actions,
+            !showSettingsButton
+                ? Container()
+                : IconButton(
+                    onPressed: () {},
+                    icon: Assets.images.icons.apps.setting.svg(),
+                  ),
+          ],
           body: Stack(
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Constants.horizontalPadding,
-                ),
+                padding: padding ??
+                    const EdgeInsets.symmetric(
+                      horizontal: Constants.horizontalPadding,
+                    ),
                 child: body,
               ),
-              Positioned(
+              if (showFooter)
+                Positioned(
                   bottom: 10,
                   left: 0,
                   right: 0,
                   child: Text(
                     "CareerCrush by Agil Setiawan",
                     textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.grey.shade400, fontSize: 12),
-                  ))
+                    style: TextStyle(
+                      color: Colors.grey.shade400,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
             ],
           ),
         );
