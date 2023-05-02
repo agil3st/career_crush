f80a52a50c204eb1d93946751d13cf56bd454861
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-02 14:32:18 +0700

    Fix bottom navbar extend body

--- lib/src/presentation/pages/index/index_screen.dart
+++ lib/src/presentation/pages/index/index_screen.dart
@@ -8,6 +8,7 @@ class IndexScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      extendBody: true,
       body: PageViewConsumer(),
       bottomNavigationBar: NavBarConsumer(),
     );
