cd9aa93286aac2247864f5f08fa7aad8750d10de
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-03 13:08:45 +0700

    Add intl library

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,17 @@ import 'package:career_crush/src/utils/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.dark),
   );
-  runApp(const ProviderScope(child: MyApp()));
+  initializeDateFormatting('id_ID', null)
+      .then((_) => {runApp(const ProviderScope(child: MyApp()))});
 }
 
 class MyApp extends StatelessWidget {
