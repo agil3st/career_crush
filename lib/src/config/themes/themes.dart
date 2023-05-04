d7724f21525bb74ff5ab299f7f3b033bf925878d
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-04 09:50:11 +0700

    Update themes.dart

--- lib/src/config/themes/themes.dart
+++ lib/src/config/themes/themes.dart
@@ -1,3 +1,4 @@
+import 'package:career_crush/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:career_crush/src/utils/constants/constants.dart';
 
@@ -6,10 +7,17 @@ class Themes {
 
   ThemeData get light => ThemeData(
         fontFamily: primaryFont,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              primary: ColorName.lavender,
+            ),
         textButtonTheme: const TextButtonThemeData(
           style: ButtonStyle(
             foregroundColor: MaterialStatePropertyAll<Color>(Colors.black),
           ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: ColorName.lavender,
+          hoverColor: ColorName.lavender,
         ),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
