65984164d1964f2c67350aae8a3ff5da63ce73ad
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-05 15:25:03 +0700

    Update event_screen.dart

--- lib/src/presentation/pages/event/event_screen.dart
+++ lib/src/presentation/pages/event/event_screen.dart
@@ -1,30 +1,21 @@
 import 'package:career_crush/src/presentation/pages/event/components/event_list.dart';
 import 'package:career_crush/src/presentation/pages/event/components/search_bar.dart';
+import 'package:career_crush/src/presentation/pages/event/components/search_buttton.dart';
 import 'package:career_crush/src/presentation/widgets/scaffolds/welcome_scaffold.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class EventScreen extends StatelessWidget {
+class EventScreen extends ConsumerWidget {
   const EventScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final showSearchBar = ref.watch(searchButtonProvider);
     return WelcomeScaffold(
       showFooter: false,
       padding: EdgeInsets.zero,
-      actions: [
-        IconButton(
-          icon: const Icon(FeatherIcons.search),
-          onPressed: () {},
-        )
-      ],
-      bottomAppBar: const PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
-          child: SearchBar(),
-        ),
-      ),
+      actions: const [SearchButton()],
+      bottomAppBar: showSearchBar ? SearchBar(autoFocus: showSearchBar) : null,
       body: const EventList(),
     );
   }
