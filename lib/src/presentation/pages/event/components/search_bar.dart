8f253ce42bb7d0edb5118752a3fc3a0bb28395fe
Agil Setiawan <agilsetiawan772@gmail.com>
2023-05-05 15:24:54 +0700

    Update search_bar.dart

--- lib/src/presentation/pages/event/components/search_bar.dart
+++ lib/src/presentation/pages/event/components/search_bar.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Search...',
-        hintStyle: const TextStyle(fontSize: 14),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: BorderSide(
-            color: Colors.grey.shade300,
-            width: 1,
+class SearchBar extends PreferredSize {
+  SearchBar({
+    super.key,
+    bool autoFocus = false,
+  }) : super(
+          preferredSize: const Size.fromHeight(70),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+            child: TextField(
+              autofocus: autoFocus,
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                hintStyle: const TextStyle(fontSize: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 1,
+                  ),
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 4,
+                ),
+              ),
+            ),
           ),
-        ),
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 12,
-          vertical: 4,
-        ),
-      ),
-    );
-  }
+        );
 }
