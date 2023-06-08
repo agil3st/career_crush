7fadbc6f09ad51c8bba9702f0c489f07d2c2a6a0
Agil Setiawan <agilsetiawan772@gmail.com>
2023-06-08 11:03:17 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,17 +14,17 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+    TestWidgetsFlutterBinding.ensureInitialized();
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final registerButton = find.widgetWithText(FilledButton, 'Register');
+    expect(registerButton, findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(registerButton);
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.textContaining('Start'), findsOneWidget);
   });
 }
